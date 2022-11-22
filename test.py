ce5ea38ca4307b758f97efcf9089b4ec940b1fc0
Dabinishere <111479186+Dabinishere@users.noreply.github.com>
2022-11-22 18:24:18 +0900

    Update test.py

--- test.py
+++ test.py
@@ -29,33 +29,34 @@ if __name__ == '__main__':
             raise KeyError(n)
 
     model.eval()
+    
+    for i in range(1, 399): # 이미지 개수
+        image = pil_image.open(args.image_file).convert('RGB')
 
-    image = pil_image.open(args.image_file).convert('RGB')
+        image_width = (image.width // args.scale) * args.scale
+        image_height = (image.height // args.scale) * args.scale
+        image = image.resize((image_width, image_height), resample=pil_image.BICUBIC)
+        image = image.resize((image.width // args.scale, image.height // args.scale), resample=pil_image.BICUBIC)
+        image = image.resize((image.width * args.scale, image.height * args.scale), resample=pil_image.BICUBIC)
+        image.save(args.image_file.replace('.', '_bicubic_x{}.'.format(args.scale)))
 
-    image_width = (image.width // args.scale) * args.scale
-    image_height = (image.height // args.scale) * args.scale
-    image = image.resize((image_width, image_height), resample=pil_image.BICUBIC)
-    image = image.resize((image.width // args.scale, image.height // args.scale), resample=pil_image.BICUBIC)
-    image = image.resize((image.width * args.scale, image.height * args.scale), resample=pil_image.BICUBIC)
-    image.save(args.image_file.replace('.', '_bicubic_x{}.'.format(args.scale)))
+        image = np.array(image).astype(np.float32)
+        ycbcr = convert_rgb_to_ycbcr(image)
 
-    image = np.array(image).astype(np.float32)
-    ycbcr = convert_rgb_to_ycbcr(image)
+        y = ycbcr[..., 0]
+        y /= 255.
+        y = torch.from_numpy(y).to(device)
+        y = y.unsqueeze(0).unsqueeze(0)
 
-    y = ycbcr[..., 0]
-    y /= 255.
-    y = torch.from_numpy(y).to(device)
-    y = y.unsqueeze(0).unsqueeze(0)
+        with torch.no_grad():
+            preds = model(y).clamp(0.0, 1.0)
 
-    with torch.no_grad():
-        preds = model(y).clamp(0.0, 1.0)
+        psnr = calc_psnr(y, preds)
+        print('PSNR: {:.2f}'.format(psnr))
 
-    psnr = calc_psnr(y, preds)
-    print('PSNR: {:.2f}'.format(psnr))
+        preds = preds.mul(255.0).cpu().numpy().squeeze(0).squeeze(0)
 
-    preds = preds.mul(255.0).cpu().numpy().squeeze(0).squeeze(0)
-
-    output = np.array([preds, ycbcr[..., 1], ycbcr[..., 2]]).transpose([1, 2, 0])
-    output = np.clip(convert_ycbcr_to_rgb(output), 0.0, 255.0).astype(np.uint8)
-    output = pil_image.fromarray(output)
-    output.save(args.image_file.replace('.', '_srcnn_x{}.'.format(args.scale)))
+        output = np.array([preds, ycbcr[..., 1], ycbcr[..., 2]]).transpose([1, 2, 0])
+        output = np.clip(convert_ycbcr_to_rgb(output), 0.0, 255.0).astype(np.uint8)
+        output = pil_image.fromarray(output)
+        output.save(args.image_file.replace('.', '_srcnn_x{}.'.format(args.scale)))
